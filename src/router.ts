d1c41e37c78c1ec0d8afb00d7be30c45181951ba
Masaya Kawabata <m-kawabata@glad-cube.com>
2018-04-15 20:49:09 +0900

    refactored

--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,36 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home.vue';
-import About from './views/About.vue';
+import Vue from 'vue'
+import Router from 'vue-router'
+import About from './front/views/About.vue'
+import Home from './front/views/Home.vue'
+import Login from './front/views/Login.vue'
+import Dashboard from './front/views/Dashboard.vue'
 
-Vue.use(Router);
+Vue.use(Router)
+
+// const mode = 'history'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: Dashboard
+  }
+]
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
-    },
-  ],
-});
+  routes
+})
